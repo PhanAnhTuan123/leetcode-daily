e8107deb970a73aa538d17a13f894a4dbdd58c33
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-04-15 22:58:10 +0700

    'pushed'

---
+++ src/MergeStringsalternately/Example1071.java
@@ -0,0 +1,5 @@
+package MergeStringsalternately;
+
+public class Example1071 {
+
+}
--- src/MergeStringsalternately/Example1768.java
+++ src/MergeStringsalternately/Example1768.java
@@ -1,5 +1,24 @@
 package MergeStringsalternately;
 
 public class Example1768 {
-
+	public static String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+        while( i < word1.length() && j < word2.length()) {
+        	merged.append(word1.charAt(i++));
+        	merged.append(word2.charAt(j++));
+        }
+        while( i <word1.length()) {
+        	merged.append(word1.charAt(i++));
+        }
+        while(j < word2.length()) {
+        	merged.append(word2.charAt(j++));
+        }
+        return merged.toString();
+	}
+	public static void main(String[] args) {
+		String word1 = "abc";
+		String word2 = "pqrs";
+		System.out.println(mergeAlternately(word1,word2));
+	}
 }
